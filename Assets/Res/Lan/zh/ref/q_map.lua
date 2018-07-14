8f03b1789f0d9c759e00e5f3cce87ce6b9d25b15
JamesOu007 <james.ou.g@gmail.com>
2018-07-14 18:00:31 +0800

    u

--- Assets/Res/Lan/zh/ref/q_map.lua
+++ Assets/Res/Lan/zh/ref/q_map.lua
@@ -3,18 +3,21 @@ return {
     ["q_name"] = "随机传送卷",
     ["q_prefab"] = "Prefabs/Scene/yland_1/base/yland_1_base.prefab",
     ["q_grid"] = "GData/Scene/Grid/yland_1.txt",
-    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt"
+    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt",
+    ["q_height"] = "GData/Scene/Height/yland_1.txt"
   },
   ["1002"] = {
     ["q_name"] = "回城卷",
     ["q_prefab"] = "Prefabs/Scene/yland_1/base/yland_1_base.prefab",
     ["q_grid"] = "GData/Scene/Grid/yland_1.txt",
-    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt"
+    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt",
+    ["q_height"] = "GData/Scene/Height/yland_1.txt"
   },
   ["1003"] = {
     ["q_name"] = "王城传送卷",
     ["q_prefab"] = "Prefabs/Scene/yland_1/base/yland_1_base.prefab",
     ["q_grid"] = "GData/Scene/Grid/yland_1.txt",
-    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt"
+    ["q_obj"] = "GData/Scene/ObjPos/yland_1.txt",
+    ["q_height"] = "GData/Scene/Height/yland_1.txt"
   }
 }
